b992f40e8377f676a958be6dfda6f46a5229c01c
rafageist <cubapush@gmail.com>
2019-06-24 21:47:16 -0500

    update

--- example/app/site/control/Blog.php
+++ example/app/site/control/Blog.php
@@ -1,24 +1,25 @@
 <?php
 
-require_once "../app/site/model/Texts.php";
-
 class Blog
 {
-	static function Latest($data, $args)
-	{
-		include "../app/site/views/header.phtml";
-		echo "<h1>Blog</h1>";
-		for($i = 1; $i < 11; $i ++) echo "- <a href=\"/blog/$i\">Entry #$i</a><br/>";
-	}
 
-	static function Entry($data, $args)
-	{
-		echo "<h2>Entry #{$args['id']}</h2>";
-		echo divWays::i("model://texts/random/paragraph/10")['paragraph'];
-	}
+  static function Latest($data, $args)
+  {
+    include "../app/site/views/header.phtml";
+    echo "<h1>Blog</h1>";
+    for ($i = 1; $i < 11; $i++) {
+      echo "- <a href=\"/blog/$i\">Entry #$i</a><br/>";
+    }
+  }
 
-	static function Comments($data, $args)
-	{
-		echo "<h2>Comments of entry #{$args['id']}</h2>";
-	}
+  static function Entry($data, $args)
+  {
+    echo "<h2>Entry #{$args['id']}</h2>";
+    echo divWays::invoke("model://texts/random/paragraph/10")['paragraph'];
+  }
+
+  static function Comments($data, $args)
+  {
+    echo "<h2>Comments of entry #{$args['id']}</h2>";
+  }
 }
